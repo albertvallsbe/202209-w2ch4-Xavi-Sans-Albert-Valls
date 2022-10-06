703514560afc73215c62dbd2c0964b5dca12f928
Albert Valls <avalls89@gmail.com>
2022-10-06 19:33:26 +0200

    Start with js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true,
-  },
-  extends: ["airbnb-base", "prettier"],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  rules: {
-    "import/extensions": ["error", "always"],
-    "lines-between-class-members": [
-      "error",
-      "always",
-      { exceptAfterSingleLine: true },
-    ],
-  },
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
+    },
+    extends: ["airbnb-base", "prettier"],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    rules: {
+        "import/extensions": ["error", "always"],
+        "lines-between-class-members": [
+            "error",
+            "always",
+            { exceptAfterSingleLine: true },
+        ],
+    },
 };
---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+class SkylabArray {
+    lenght;
+    push;
+    some;
+    find;
+    filter;
+    map;
+
+    constructor(...rest) {
+        this.rest = rest;
+    }
+}
+
+const provapepe = new SkylabArray();
+
+console.log(provapepe);
