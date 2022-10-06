b4f3703ee032244f8b94ba658f71947158740017
Xavier Sans <xavi.sans.b@gmail.com>
2022-10-06 21:06:06 +0200

    Fix length test and index.html title

--- public/js/SkylabArray/SkylabArray.test.js
+++ public/js/SkylabArray/SkylabArray.test.js
@@ -1,16 +1,16 @@
 import SkylabArray from "./SkylabArray.js";
 
 describe("Given the function SkaylabArray ", () => {
-    describe("When it receives 1,2", () => {
-        test("Then it should return 2", () => {
-            const value = 1;
-            const value2 = 2;
-            const expectedResult = 2;
+  describe("When it receives 1,2", () => {
+    test("Then it should return 2", () => {
+      const value = 1;
+      const value2 = 2;
+      const expectedResult = 2;
 
-            const object = SkylabArray(value, value2)
-            const objectMethod = object.countLength();
+      const object = new SkylabArray(value, value2);
+      const objectMethod = object.countLength();
 
-            expect(objectMethod).toBe(expectedResult);
-        })
-    })
-})
+      expect(objectMethod).toBe(expectedResult);
+    });
+  });
+});
