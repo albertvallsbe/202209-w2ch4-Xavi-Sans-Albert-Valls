17bea0988909a17222f5deb89f641835e61bd061
Xavier Sans <xavi.sans.b@gmail.com>
2022-10-06 20:50:27 +0200

    Add push method

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,6 @@ import SkylabArray from "./SkylabArray/SkylabArray.js";
 
 const numbers = new SkylabArray(1, 2, 3, 4, 5);
 
+numbers.addPush();
+numbers.countLength();
 console.log(numbers.countLength());
